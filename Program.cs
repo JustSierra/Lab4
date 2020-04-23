ec207365806aa0e320a306b4f91817054d5bd12b
Sierra Johnson <Bash0704>
2020-04-23 09:43:57 -0400

    Lab4

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+
+
+
+
+            for (int i = 0; i < length; i++)
+            {
+                //Loop goes here....
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Would you like to continue? y/n ");
+            string response = Console.ReadLine();
+            if (response == "y")
+            {
+                Console.WriteLine("Enter another number");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
